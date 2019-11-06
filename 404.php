cd5661713f5101348a486ba0f5a8513e29a9cebb
parmentlovski <parmentelot.bryan@hotmail.fr>
2019-11-06 15:50:56 +0100

    ajout htpasswd, creation page 404 et debut responsive

--- 404.php
+++ 404.php
@@ -1,6 +1,14 @@
-<main>
-    <h1>ERROR</h1>
-    <?php  wp_nav_menu();?>
+<?php do_action('display_header', 'bysshe_header'); ?>
+
+<main id="page-error"  class="display-menu">
+    <section class="container">
+        <h1><span>ERREUR 404</span></h1>
+        <div class="row">
+            <p class="col-12 text-center">Page introuvable : dirigez vous vers le menu pour voir ou se passe la musique !</p>
+            <img class="col-12 img-fluid" src="wp-content/themes/bysshe/assets/img/bysshe-404.jpg">
+        </div>
+
+    </section>
 </main>
 
-<?php require('template-page.php'); ?>
+<?php do_action('display_footer', 'bysshe_footer'); ?>
